b233700db7c478526d594821307241c9a4a67ddd
shinho kang <shinho.kang@insighton.kr>
2021-12-07 01:27:43 +0900

    format

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,19 +1,19 @@
-import { PrismaClient, User } from "@prisma/client";
+import { PrismaClient, User } from '@prisma/client'
 
 type Context = {
-  loggedInUser: User | null;
-  client: PrismaClient;
-};
+  loggedInUser: User | null
+  client: PrismaClient
+}
 
 export type Resolver = (
   root: any,
   args: any,
   context: Context,
   info: any
-) => any;
+) => any
 
 export type Resolvers = {
   [key: string]: {
-    [key: string]: Resolver;
-  };
-};
+    [key: string]: Resolver
+  }
+}
